84bb0f57407899c955585346afe9eef6eca499fa
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-06-09 11:01:59 -0300

    signout button

--- frontend/src/components/SideBar/index.js
+++ frontend/src/components/SideBar/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './index.css'
+import { UilSignout } from '@iconscout/react-unicons'
 
 function SideBar() {
   return (
       <div className="SideBar">
-
+        <nav className="signout-container p-2 mt-1">
+          <UilSignout size="70" className="signout-icon"/>
+          <label className="signout-text">SIGNOUT</label>
+        </nav>
+        
       </div>
   );
 }
